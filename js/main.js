b1395504113eaff6866fb08cbb8ade724dfa7e49
Rafael Grillo <grillorafael@gmail.com>
2014-04-06 20:02:57 -0300

    refatorando e adicionando seleção de peças

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-var Chess;
+function Chess() {
+  _self = this;
+  _self.selectedPiece = null;
+  _self.table = [
+    ['T', 'N', 'B', 'Q', 'K', 'B', 'N', 'T'],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
+  ];
 
-Chess = (function() {
-  function Chess() {
-    this.table = [
-      ['T', 'N', 'B', 'Q', 'K', 'B', 'N', 'T'],
-      ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-      ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
-    ];
+  _self.handlePieceClick = function(e) {
+    if(_self.selectedPiece == null) {
+      $(this).addClass('selected');
+      _self.selectedPiece = $(this);
+    }
+    else if($(this).is(_self.selectedPiece)) {
+      $(this).removeClass('selected');
+      _self.selectedPiece = null;
+    }
+
+    return false;
   }
 
-  return Chess;
-
-})();
-
+  $('.piece').click(_self.handlePieceClick);
+};
 
 var chess = new Chess();
