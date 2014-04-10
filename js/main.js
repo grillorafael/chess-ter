dbb156024444d5d81577d609f110e279cc15c999
Rafael Grillo <grillorafael@gmail.com>
2014-04-10 15:50:46 -0300

    adding pawn movements CLOSES #243

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 function Chess() {
-  console.log('Starting Chess...');
   _self = this;
   _self.selectedPiece = null;
   _self.table = [
@@ -12,17 +11,13 @@ function Chess() {
     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
     ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
   ];
-  console.log('table', _self.table);
 
   _self.unselectPiece = function() {
-    console.log('unselectPiece');
     _self.selectedPiece.removeClass('selected');
     _self.selectedPiece = null;
   };
 
   _self.handlePieceClick = function(e) {
-    console.log('handlePieceClick');
-
     // it selects a piece
     if(_self.selectedPiece == null) {
       $(this).addClass('selected');
@@ -42,7 +37,6 @@ function Chess() {
   };
 
   _self.handleSquareClick = function(e) {
-    console.log('handleSquareClick');
     var tdPosition = _self.getCellPosition($(this));
 
     if(_self.selectedPiece != null) {
@@ -52,31 +46,26 @@ function Chess() {
   };
 
   _self.getPiecePosition = function(piece) {
-    console.log('getPiecePosition');
     if(piece === undefined) {
       piece = _self.selectedPiece;
     }
     var pieceTd = piece.parents('td');
     var piecePosition = _self.getCellPosition(pieceTd);
-    console.log('piece position', piecePosition);
     return piecePosition;
   };
 
   _self.getCellPosition = function(cell) {
-    console.log('getCellPosition');
     var column = cell[0].cellIndex;
     var row = cell[0].parentNode.rowIndex;
     var tdPosition = [row, column];
-    console.log('cell position', tdPosition);
     return tdPosition;
   };
 
   _self.moveFromTo = function(from, to) {
-    console.log('moveFromTo');
+    if(!_self.canMoveFromTo(from, to)) return;
+
     var piece = $('#' + from[0] + '' + from[1]).find('.piece');
     var cell = $('#' + to[0] + '' + to[1]);
-    console.log('moving', piece);
-    console.log('to', cell);
 
     _self.table[to[0]][to[1]] = _self.table[from[0]][from[1]];
     _self.table[from[0]][from[1]] = '';
@@ -85,8 +74,72 @@ function Chess() {
     cell.append(piece);
 
     _self.unselectPiece();
+  };
 
-    console.log('new table', _self.table);
+  _self.canMoveFromTo = function(from, to) {
+    var possibleMovements = _self.possibleMovementsFor(from);
+    var totalMovements = possibleMovements.length;
+    for(var i = 0; i < totalMovements; i++) {
+      var actualMovement = possibleMovements[i];
+      if(actualMovement[0] == to[0] && actualMovement[1] == to[1]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  _self.possibleMovementsFor = function(piecePosition) {
+    switch(_self.table[piecePosition[0]][piecePosition[1]].toLowerCase()){
+    case 'p':
+      return _self.possibleMovementsForPawn(piecePosition);
+    }
+  }
+
+  _self.possibleMovementsForPawn = function(piecePosition) {
+    var piece = _self.table[piecePosition[0]][piecePosition[1]];
+    var possibleMovements = [];
+    if(piece == piece.toLowerCase()) {
+      if(_self.table[piecePosition[0] - 1][piecePosition[1]] == '') {
+        possibleMovements.push([piecePosition[0] - 1, piecePosition[1]]);
+      }
+      if(piecePosition[0] == 6 && _self.table[piecePosition[0] - 2][piecePosition[1]] == '' && possibleMovements.length == 1) {
+        possibleMovements.push([piecePosition[0] - 2, piecePosition[1]]);
+      }
+
+      try {
+        if(_self.table[piecePosition[0] - 1][piecePosition[1] + 1] != '' && _self.table[piecePosition[0] - 1][piecePosition[1] + 1].toUpperCase() == _self.table[piecePosition[0] - 1][piecePosition[1] + 1]) {
+          possibleMovements.push([piecePosition[0] - 1, piecePosition[1] + 1]);
+        }
+      } catch(e) {};
+
+      try {
+        if(_self.table[piecePosition[0] - 1][piecePosition[1] - 1] != '' && _self.table[piecePosition[0] - 1][piecePosition[1] - 1].toUpperCase() == _self.table[piecePosition[0] - 1][piecePosition[1] - 1]) {
+          possibleMovements.push([piecePosition[0] - 1, piecePosition[1] - 1]);
+        }
+      }catch (e) {};
+    }
+    else {
+      if(_self.table[piecePosition[0] + 1][piecePosition[1]] == '') {
+        possibleMovements.push([piecePosition[0] + 1, piecePosition[1]]);
+      }
+      if(piecePosition[0] == 1 && _self.table[piecePosition[0] + 2][piecePosition[1]] == '' && possibleMovements.length == 1) {
+        possibleMovements.push([piecePosition[0] + 2, piecePosition[1]]);
+      }
+
+      try {
+        if(_self.table[piecePosition[0] + 1][piecePosition[1] + 1] != '' && _self.table[piecePosition[0] + 1][piecePosition[1] + 1].toLowerCase() == _self.table[piecePosition[0] + 1][piecePosition[1] + 1]) {
+          possibleMovements.push([piecePosition[0] + 1, piecePosition[1] + 1]);
+        }
+      } catch(e) {};
+
+      try {
+        if(_self.table[piecePosition[0] + 1][piecePosition[1] - 1] != '' && _self.table[piecePosition[0] + 1][piecePosition[1] - 1].toLowerCase() == _self.table[piecePosition[0] + 1][piecePosition[1] - 1]) {
+          possibleMovements.push([piecePosition[0] + 1, piecePosition[1] - 1]);
+        }
+      }catch (e) {};
+    }
+
+    return possibleMovements;
   };
 
   // Adding event listeners
