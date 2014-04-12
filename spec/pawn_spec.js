977e19e5e898a50e7d6c7424174abe39f145b542
Rafael Grillo <grillorafael@gmail.com>
2014-04-12 16:13:27 -0300

    pawn specs

--- spec/pawn_spec.js
+++ spec/pawn_spec.js
@@ -1,19 +1,84 @@
 describe('Pawn', function(){
-  var table;
-  beforeEach(function(){
-    table = [
-      ['T', 'N', 'B', 'Q', 'K', 'B', 'N', 'T'],
-      ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-      ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
-    ];
-  });
-
   describe('.possibleMovements', function(){
+    describe('first movement', function(){
+      beforeEach(function(){
+        initialTable = [
+          ['T', 'N', 'B', 'Q', 'K', 'B', 'N', 'T'],
+          ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+          ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
+        ];
+      });
 
+      describe('black pieces', function(){
+        it('moves 1 row down', function(){
+          var movements = Pawn.possibleMovements([1, 0], initialTable);
+          expect(movements).toContain([2, 0]);
+        });
+        it('moves 2 rows down', function(){
+          var movements = Pawn.possibleMovements([1, 0], initialTable);
+          expect(movements).toContain([3, 0]);
+        });
+      });
+      describe('white pieces', function(){
+        it('moves 1 row up', function(){
+          var movements = Pawn.possibleMovements([6, 0], initialTable);
+          expect(movements).toContain([5, 0]);
+        });
+        it('moves 2 rows up', function(){
+          var movements = Pawn.possibleMovements([6, 0], initialTable);
+          expect(movements).toContain([4, 0]);
+        });
+      });
+    });
+    describe('mid game movement', function(){
+      beforeEach(function(){
+        midGameTable = [
+          ['T', 'N', 'B', 'Q', 'K', 'B', 'N', 'T'],
+          ['', '', '', 'P', 'P', 'P', '', 'P'],
+          ['', '', '', '', '', '', '', ''],
+          ['P', 'P', 'P', '', '', '', 'P', ''],
+          ['', 'p', '', '', '', 'p', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['p', '', 'p', 'p', '', 'p', 'p', 'p'],
+          ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
+        ];
+      });
+      describe('black pieces', function(){
+        describe('empty square', function(){
+          it('moves only 1 row down', function(){
+            var movements = Pawn.possibleMovements([3, 2], midGameTable);
+            expect(movements).toContain([4, 2]);
+            expect(movements).not.toContain([5, 2]);
+          });
+        });
+        describe('enemy at diagonal', function(){
+          it('moves only towards enemy', function(){
+            var movements = Pawn.possibleMovements([3, 6], midGameTable);
+            expect(movements).toContain([4, 5]);
+          });
+        });
+      });
+      describe('white pieces', function(){
+        describe('empty square', function(){
+          it('moves only 1 row up', function(){
+            var movements = Pawn.possibleMovements([4, 5], midGameTable);
+            expect(movements).toContain([3, 5]);
+            expect(movements).not.toContain([2, 5]);
+          });
+        });
+        describe('enemy at diagonal', function(){
+          it('moves only towards enemy', function(){
+            var movements = Pawn.possibleMovements([4, 1], midGameTable);
+            expect(movements).toContain([3, 0]);
+            expect(movements).toContain([3, 2]);
+          });
+        });
+      });
+    });
   });
 });
