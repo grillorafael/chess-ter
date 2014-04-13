83dfe5ec0348ded985e0be192666cc708d068a10
Ian <ianvilar@id.uff.br>
2014-04-13 14:56:37 -0300

    Refeitas as movimentações da rainha, bispo e torre com o novo formato

--- js/queen.js
+++ js/queen.js
@@ -2,26 +2,153 @@
 var Queen = (function(){});
 Queen.possibleMovements = function(piecePosition, targetSquare, table) {
   var possibleMovements = [];
-  try {
-    if(TableUtil.hasMovedLowerRightDiagonally(piecePosition, targetSquare) && !TableUtil.hasLowerRightDiagonalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, targetSquare, table)) {
-      possibleMovements.push(targetSquare);
-    }
-    else if(TableUtil.hasMovedLowerLeftDiagonally(piecePosition, targetSquare) && !TableUtil.hasLowerLeftDiagonalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, targetSquare, table)) {
-      possibleMovements.push(targetSquare);
-    }
-    else if(TableUtil.hasMovedUpperRightDiagonally(piecePosition, targetSquare) && !TableUtil.hasUpperRightDiagonalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, targetSquare, table)) {
-      possibleMovements.push(targetSquare);
-    }
-    else if(TableUtil.hasMovedUpperLeftDiagonally(piecePosition, targetSquare) && !TableUtil.hasUpperLeftDiagonalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, targetSquare, table)) {
-      possibleMovements.push(targetSquare);
-    }
-    else if(!TableUtil.hasVerticalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, [targetSquare[0], piecePosition[1]], table)) {
-      possibleMovements.push([targetSquare[0], piecePosition[1]]);
-    }
-    else if(!TableUtil.hasHorizontalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, [piecePosition[0], targetSquare[1]], table)) {
-      possibleMovements.push([piecePosition[0], targetSquare[1]]);
-    }
-  } catch(e) {}
+  
+  /* INÍCIO DOS POSSÍVEIS MOVIMENTOS HORIZONTAIS E VERTICAIS */
+  var tmpPosition = piecePosition;
+  var hasValidDownVerticalPositions = true;
+  while(hasValidDownVerticalPositions)
+  {
+    var newPostion = [tmpPosition[0] + 1, tmpPosition[1]];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
 
+      else
+      { 
+        hasValidDownVerticalPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidDownVerticalPositions = false};
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidUpVerticalPositions = true;
+  while(hasValidUpVerticalPositions)
+  {
+    var newPostion = [tmpPosition[0] - 1, tmpPosition[1]];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
+
+      else
+      { 
+        hasValidUpVerticalPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidUpVerticalPositions = false};
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidHorizontalRightPositions = true;
+  while(hasValidHorizontalRightPositions)
+  {
+    var newPostion = [tmpPosition[0], tmpPosition[1] + 1];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
+
+      else
+      { 
+        hasValidHorizontalRightPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidHorizontalRightPositions = false};
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidHorizontalLeftPositions = true;
+  while(hasValidHorizontalLeftPositions)
+  {
+    var newPostion = [tmpPosition[0], tmpPosition[1] - 1];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
+
+      else
+      { 
+        hasValidHorizontalLeftPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidHorizontalLeftPositions = false};
+  }
+/* FIM DOS POSSÍVEIS MOVIMENTOS HORIZONTAIS E VERTICAIS */
+
+/* INÍCIO DOS POSSÍVEIS MOVIMENTOS DIAGONAIS */
+  tmpPosition = piecePosition;
+  var hasValidRightUpPositions = true;
+  while(hasValidRightUpPositions) {
+    var newPosition = [tmpPosition[0] - 1, tmpPosition[1] + 1];
+    try {
+      if(TableUtil.validSquare(tmpPosition, newPosition, table)) {
+        possibleMovements.push(newPosition);
+      }
+      else {
+        hasValidRightUpPositions = false;
+      }
+    } catch(e) { hasValidRightUpPositions = false; }
+    tmpPosition = newPosition;
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidRightDownPositions = true;
+  while(hasValidRightDownPositions) {
+    var newPosition = [tmpPosition[0] + 1, tmpPosition[1] + 1];
+    try {
+      if(TableUtil.validSquare(piecePosition, newPosition, table)) {
+        possibleMovements.push(newPosition);
+      }
+      else {
+        hasValidRightDownPositions = false;
+      }
+    } catch(e) { hasValidRightDownPositions = false; }
+    tmpPosition = newPosition;
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidLeftDownPositions = true;
+  while(hasValidLeftDownPositions) {
+    var newPosition = [tmpPosition[0] + 1, tmpPosition[1] - 1];
+    try {
+      if(TableUtil.validSquare(piecePosition, newPosition, table)) {
+        possibleMovements.push(newPosition);
+      }
+      else {
+        hasValidLeftDownPositions = false;
+      }
+    } catch(e) { hasValidLeftDownPositions = false; }
+    tmpPosition = newPosition;
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidLeftUpPositions = true;
+  while(hasValidLeftUpPositions) {
+    var newPosition = [tmpPosition[0] - 1, tmpPosition[1] - 1];
+    try {
+      if(TableUtil.validSquare(piecePosition, newPosition, table)) {
+        possibleMovements.push(newPosition);
+      }
+      else {
+        hasValidLeftUpPositions = false;
+      }
+    } catch(e) { hasValidLeftUpPositions = false; }
+    tmpPosition = newPosition;
+  }
+/* FIM DOS POSSÍVEIS MOVIMENTOS DIAGONAIS */
   return possibleMovements;
 };
--- js/tower.js
+++ js/tower.js
@@ -2,17 +2,90 @@
 var Tower = (function(){});
 Tower.possibleMovements = function(piecePosition, targetSquare, table) {
   var possibleMovements = [];
-  try {
-    if(!TableUtil.hasVerticalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, [targetSquare[0], piecePosition[1]], table)) {
-      possibleMovements.push([targetSquare[0], piecePosition[1]]);
-    }
-  } catch(e) {}
+  
+  var tmpPosition = piecePosition;
+  var hasValidDownVerticalPositions = true;
+  while(hasValidDownVerticalPositions)
+  {
+    var newPostion = [tmpPosition[0] + 1, tmpPosition[1]];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
 
-  try {
-    if(!TableUtil.hasHorizontalCollision(piecePosition, targetSquare) && TableUtil.validSquare(piecePosition, [piecePosition[0], targetSquare[1]], table)) {
-      possibleMovements.push([piecePosition[0], targetSquare[1]]);
-    }
-  } catch(e) {}
+      else
+      { 
+        hasValidDownVerticalPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidDownVerticalPositions = false};
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidUpVerticalPositions = true;
+  while(hasValidUpVerticalPositions)
+  {
+    var newPostion = [tmpPosition[0] - 1, tmpPosition[1]];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
+
+      else
+      { 
+        hasValidUpVerticalPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidUpVerticalPositions = false};
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidHorizontalRightPositions = true;
+  while(hasValidHorizontalRightPositions)
+  {
+    var newPostion = [tmpPosition[0], tmpPosition[1] + 1];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
+
+      else
+      { 
+        hasValidHorizontalRightPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidHorizontalRightPositions = false};
+  }
+
+  tmpPosition = piecePosition;
+  var hasValidHorizontalLeftPositions = true;
+  while(hasValidHorizontalLeftPositions)
+  {
+    var newPostion = [tmpPosition[0], tmpPosition[1] - 1];
+    try
+    {
+      if(TableUtil.validSquare(tmpPosition, newPostion, table)) 
+      {
+        possibleMovements.push(newPostion);
+      }
+
+      else
+      { 
+        hasValidHorizontalLeftPositions = false;
+      }
+
+      tmpPosition = newPostion;
+    } catch(e) {hasValidHorizontalLeftPositions = false};
+  }
 
   return possibleMovements;
 };
