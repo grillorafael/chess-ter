a8e22268b4a67942ba222c2d1486b8ae3e0fc7b0
Rafael Grillo <grillorafael@gmail.com>
2014-04-16 00:36:23 -0300

    new piece prototype

--- js/piece/piece.js
+++ js/piece/piece.js
@@ -4,11 +4,19 @@ function Piece(player) {
     //TODO: Fazer verificação se player é uma instancia da classe player
     return player;
   };
+
+  this.empty = function() {
+    return false;
+  }
 };
 
 
-Piece.prototype.possibleMovements = function (position) {
+Piece.prototype.possibleMovements = function (position, board) {
   var msg = "Piece#possibleMovements: Not yet implemented";
   alert(msg);
   throw msg;
+};
+
+Piece.prototype.isEnemyOf = function(pi) {
+  return !((pi.player().isBlack() && this.player().isBlack()) || (pi.player().isWhite() && this.player().isWhite()));
 };
