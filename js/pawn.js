de8ced5af803dd56e351e3b53db9b7914204cd89
Rafael Grillo <grillorafael@gmail.com>
2014-04-16 00:37:21 -0300

    adding a work in progress version of pawn.

--- js/pawn.js
+++ js/pawn.js
@@ -2,20 +2,60 @@ function Pawn(player) {
   Piece.call(this, player);
 };
 
-
-Pawn.prototype = new Pawn();
+Pawn.prototype = new Piece();
 Pawn.prototype.constructor = Pawn;
 
-
-Pawn.prototype.possibleMovements = function (position) {
-  if(position instanceof BoardPosition) {
-    var possibleMovements = [];
-
-    return possibleMovements;
-  }
-  else {
+Pawn.prototype.possibleMovements = function (position, board) {
+  if(!(position instanceof BoardPosition)) {
     var msg = "Pawn#possibleMovements: position should be a BoardPosition";
     alert(msg);
-    throw msg;
+    throw Error(msg);
+  };
+
+  var possibleMovements = [];
+
+  if(this.player().isWhite()) {
+    if(position.line() == 2 && board.at(position.nextLine().nextLine()).empty()) {
+      possibleMovements.push(position.nextLine().nextLine());
+    }
+
+    if(board.at(position.nextLine()).empty()) {
+      possibleMovements.push(position.nextLine());
+    }
+
+    // rightup
+    var rightUp = board.at(position.nextLine().nextColumn());
+    if(!rightUp.empty() && this.isEnemyOf(rightUp)) {
+      possibleMovements.push(position.nextLine().nextColumn());
+    }
+
+    // lefttup
+    var leftUp = board.at(position.nextLine().previousColumn());
+    if(!leftUp.empty() && this.isEnemyOf(leftUp)) {
+      possibleMovements.push(position.nextLine().previousColumn());
+    }
   }
+  else {
+    if(position.line() == 7 && board.at(position.previousLine().previousLine()).empty()) {
+      possibleMovements.push(position.previousLine().previousLine());
+    }
+
+    if(board.at(position.previousLine()).empty()) {
+      possibleMovements.push(position.previousLine());
+    }
+
+    // rightup
+    var rightDown = board.at(position.previousLine().nextColumn());
+    if(!rightDown.empty() && this.isEnemyOf(rightDown)) {
+      possibleMovements.push(position.previousLine().nextColumn());
+    }
+
+    // lefttup
+    var leftDown = board.at(position.previousLine().previousColumn());
+    if(!leftDown.empty() && this.isEnemyOf(leftUp)) {
+      possibleMovements.push(position.previousLine().previousColumn());
+    }
+  }
+
+  return possibleMovements;
 };
