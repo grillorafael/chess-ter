6d1c282b5cb4656690ed61ba9fc700dd2eabbff6
Rafael Grillo <grillorafael@gmail.com>
2014-04-17 00:53:21 -0300

    testes da torre e movimentação da mesma

--- js/tower.js
+++ js/tower.js
@@ -6,16 +6,94 @@ function Tower(player) {
 Tower.prototype = new Piece();
 Tower.prototype.constructor = Tower;
 
-
-Tower.prototype.possibleMovements = function (position) {
-  if(position instanceof BoardPosition) {
-    var possibleMovements = [];
-
-    return possibleMovements;
-  }
-  else {
+Tower.prototype.possibleMovements = function (position, board) {
+  if(!(position instanceof BoardPosition)) {
     var msg = "Tower#possibleMovements: position should be a BoardPosition";
     alert(msg);
-    throw msg;
+    throw Error(msg);
+  };
+
+  var hasNextLine = true, hasPreviousLine = true, hasNextColumn = true, hasPreviousColumn = true, tmpPosition = position.nextLine(), possibleMovements = [], lastPosition = position;
+  while(hasNextLine) {
+    if(tmpPosition == lastPosition) {
+      hasNextLine = false;
+    }
+    else if(board.at(tmpPosition).empty()) {
+      possibleMovements.push(tmpPosition);
+    }
+    else {
+      if(this.isEnemyOf(board.at(tmpPosition))) {
+        possibleMovements.push(tmpPosition);
+      }
+      hasNextLine = false;
+    }
+
+    lastPosition = tmpPosition;
+    tmpPosition = tmpPosition.nextLine();
   }
+
+  tmpPosition = position.previousLine();
+  lastPosition = position;
+  while(hasPreviousLine) {
+    if(tmpPosition == lastPosition) {
+      hasPreviousLine = false;
+    }
+    else if(board.at(tmpPosition).empty()) {
+      possibleMovements.push(tmpPosition);
+    }
+    else {
+      if(this.isEnemyOf(board.at(tmpPosition))) {
+        possibleMovements.push(tmpPosition);
+      }
+      hasPreviousLine = false;
+    }
+
+    lastPosition = tmpPosition;
+    tmpPosition = tmpPosition.previousLine();
+  }
+
+  tmpPosition = position.nextColumn();
+  lastPosition = position;
+  while(hasNextColumn) {
+    if(tmpPosition == lastPosition) {
+      hasNextColumn = false;
+    }
+    else if(board.at(tmpPosition).empty()) {
+      possibleMovements.push(tmpPosition);
+    }
+    else {
+      if(this.isEnemyOf(board.at(tmpPosition))) {
+        possibleMovements.push(tmpPosition);
+      }
+      hasNextColumn = false;
+    }
+
+    lastPosition = tmpPosition;
+    tmpPosition = tmpPosition.nextColumn();
+  }
+
+
+  tmpPosition = position.previousColumn();
+  lastPosition = position;
+  while(hasPreviousColumn) {
+    if(tmpPosition == lastPosition) {
+      hasPreviousColumn = false;
+    }
+    else if(board.at(tmpPosition).empty()) {
+      possibleMovements.push(tmpPosition);
+    }
+    else {
+      if(this.isEnemyOf(board.at(tmpPosition))) {
+        possibleMovements.push(tmpPosition);
+      }
+      hasPreviousColumn = false;
+    }
+
+    lastPosition = tmpPosition;
+    tmpPosition = tmpPosition.previousColumn();
+  }
+
+
+
+  return possibleMovements;
 };
--- spec/tower_spec.js
+++ spec/tower_spec.js
@@ -1,19 +1,90 @@
 describe('Tower', function(){
-  var table;
+  var board, blackPlayer, whitePlayer, blackTower, whiteTower;
   beforeEach(function(){
-    table = [
-      ['T', 'N', 'B', 'Q', 'K', 'B', 'N', 'T'],
-      ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', ''],
-      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-      ['t', 'n', 'b', 'q', 'k', 'b', 'n', 't']
-    ];
+    blackPlayer = new Player(Player.BLACK);
+    whitePlayer = new Player(Player.WHITE);
+
+    blackTower = new Tower(blackPlayer);
+    whiteTower = new Tower(whitePlayer);
   });
 
   describe('.possibleMovements', function(){
+    it('moves to a upper line until find a enemy', function(){
+      board = new Board('rnbqkbnr/pppppppp/8/8/8/8/1PPPPPPP/RNBQKBNR');
+      var movements = whiteTower.possibleMovements(new BoardPosition('a1'), board);
+      var expected = [
+        new BoardPosition('a2'),
+        new BoardPosition('a3'),
+        new BoardPosition('a4'),
+        new BoardPosition('a5'),
+        new BoardPosition('a6'),
+        new BoardPosition('a7')
+      ];
 
+      compareMovementsAndExpected(movements, expected);
+    });
+
+    it('moves to a previous line until find a enemy', function(){
+      board = new Board('rnbqkbnr/1ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
+      var movements = blackTower.possibleMovements(new BoardPosition('a8'), board);
+      var expected = [
+        new BoardPosition('a7'),
+        new BoardPosition('a6'),
+        new BoardPosition('a5'),
+        new BoardPosition('a4'),
+        new BoardPosition('a3'),
+        new BoardPosition('a2')
+      ];
+
+      compareMovementsAndExpected(movements, expected);
+    });
+
+    it('moves to right column until find a enemy', function(){
+      board = new Board('1nbqkbnr/pppppppp/r7/8/8/8/PPPPPPPP/RNBQKBNR');
+      var movements = blackTower.possibleMovements(new BoardPosition('a6'), board);
+      var expected = [
+        new BoardPosition('b6'),
+        new BoardPosition('c6'),
+        new BoardPosition('d6'),
+        new BoardPosition('e6'),
+        new BoardPosition('f6'),
+        new BoardPosition('g6'),
+        new BoardPosition('h6'),
+        new BoardPosition('a5'),
+        new BoardPosition('a4'),
+        new BoardPosition('a3'),
+        new BoardPosition('a2')
+      ];
+
+      compareMovementsAndExpected(movements, expected);
+    });
+
+    it('moves to left column until find a enemy', function(){
+      board = new Board('rnbqkbn1/pppppppp/7r/8/8/8/PPPPPPPP/RNBQKBNR');
+      var movements = blackTower.possibleMovements(new BoardPosition('h6'), board);
+      var expected = [
+        new BoardPosition('a6'),
+        new BoardPosition('b6'),
+        new BoardPosition('c6'),
+        new BoardPosition('d6'),
+        new BoardPosition('e6'),
+        new BoardPosition('f6'),
+        new BoardPosition('g6'),
+        new BoardPosition('h5'),
+        new BoardPosition('h4'),
+        new BoardPosition('h3'),
+        new BoardPosition('h2')
+      ];
+
+      compareMovementsAndExpected(movements, expected);
+    });
   });
+
+  function compareMovementsAndExpected(movements, expected) {
+    expect(movements.length).toEqual(expected.length);
+
+    for (var i = 0, l = expected.length; i < l; i++) {
+      expect(movements).toContain(expected[i])
+    }
+  }
 });
