0c30da6859016c6173fb24603aab7e8874e82ecd
Rafael Grillo <grillorafael@gmail.com>
2014-05-09 13:54:22 -0300

    adicionando coverage CLOSES #196 #202

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,44 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     jasmine: {
-      src: [
-        'js/pieces/piece/*.js',
-        'js/pieces/*.js',
-        'js/board_position.js',
-        'js/board.js',
-        'js/player.js',
-        'js/main.js',
-        'js/scripts.js'
-      ],
-      options: {
-        vendor: 'js/vendor/*.js',
-        specs: 'spec/*_spec.js',
-        helpers: 'spec/*_helper.js'
+    	coverage: {
+        src: [
+          'js/pieces/piece/*.js',
+          'js/pieces/*.js',
+          'js/board_position.js',
+          'js/board.js',
+          'js/player.js',
+          'js/main.js',
+          'js/scripts.js'
+        ],
+        options: {
+          vendor: 'js/vendor/*.js',
+          specs: 'spec/*_spec.js',
+          template: require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+            requireConfig: {
+              baseUrl: '.grunt/grunt-contrib-jasmine/src/main/js/'
+            },
+            coverage: 'coverage/coverage.json',
+            report: [
+              {
+                type: 'html',
+                options: {
+                  dir: 'coverage/html'
+                }
+              },
+              {
+                type: 'cobertura',
+                options: {
+                  dir: 'coverage/cobertura'
+                }
+              },
+              {
+                type: 'text-summary'
+              }
+            ]
+          }
+        }
       }
     },
     watch: {
@@ -53,5 +78,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-inline');
+  grunt.registerTask('coverage', 'jasmine:istanbul');
+  grunt.registerTask('test:coverage', ['jasmine:coverage']);
   grunt.registerTask('default', ['watch']);
 };
